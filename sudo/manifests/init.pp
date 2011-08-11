7ca8989fd688477290f95b843e2d78a10f4bdecd
Mark Bennett <mark@burmis.ca>
2011-08-11 00:45:22 +0000

    Initial check-in

---
+++ sudo/manifests/init.pp
@@ -0,0 +1,20 @@
+class sudo {
+  package { sudo:
+    ensure => present,
+  }
+
+  if $operatingsystem == "Ubuntu" {
+    package { "sudo-ldap":
+      ensure => present,
+      require => Package["sudo"],
+    }
+  }
+
+  file { "/etc/sudoers":
+    owner => "root",
+    group => "root",
+    mode => 0400,
+    source => "puppet://$puppetserver/modules/sudo/etc/sudoers",
+    require => Package["sudo"] 
+  }
+}
